9b1c65b02da04a3e18a3825891d6df419ba3dce4
shochmann <shochmann@yahoo.com>
2020-11-29 23:05:18 -0600

    making GenreScreen a class instead of a function

--- screens/GenreScreen.tsx
+++ screens/GenreScreen.tsx
@@ -1,22 +1,33 @@
 import * as React from 'react';
-import { Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, View } from '../components/Themed';
+const blah = ["hello"];
 
-export default function GenreScreen() {
-  return (
+class GenreScreen extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      loading: false,
+      dataSource: [],
+      items: []
+    };
+  }
+
+  render() {
+    return (
     <View style={styles.container}>
-      <Text style={styles.title}>Please Choose a Genre</Text>
+      <Text style={styles.title}>Please Choose a Gene</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <View style={{flexDirection:'row'}}>
         <TouchableOpacity
           style={styles.button}
-          onPress={GetAction}
+          onPress={this.GetAction.bind(this)}
         >
           <Text>Action</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.button}
-          onPress={GetAction}
+          onPress={this.GetAction.bind(this)}
         >
           <Text>Horror</Text>
         </TouchableOpacity>
@@ -24,13 +35,13 @@ export default function GenreScreen() {
       <View style={{flexDirection:'row'}}>
       <TouchableOpacity
           style={styles.button}
-          onPress={GetAction}
+          onPress={this.GetAction.bind(this)}
         >
           <Text>Documentaries</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.button}
-          onPress={GetAction}
+          onPress={this.GetAction.bind(this)}
         >
           <Text>Family</Text>
         </TouchableOpacity>
@@ -38,13 +49,13 @@ export default function GenreScreen() {
       <View style={{flexDirection:'row'}}>
       <TouchableOpacity
           style={styles.button}
-          onPress={GetAction}
+          onPress={this.GetAction.bind(this)}
         >
           <Text>Documentaries</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.button}
-          onPress={GetAction}
+          onPress={this.GetAction.bind(this)}
         >
           <Text>Family</Text>
         </TouchableOpacity>
@@ -52,24 +63,70 @@ export default function GenreScreen() {
       <View style={{flexDirection:'row'}}>
       <TouchableOpacity
           style={styles.button}
-          onPress={GetAction}
+          onPress={this.GetAction.bind(this)}
         >
           <Text>Documentaries</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.button}
-          onPress={GetAction}
+          onPress={this.GetAction.bind(this)}
         >
           <Text>Family</Text>
         </TouchableOpacity>
       </View>
-    </View>
-  );
+      <FlatList
+                    data={this.state.dataSource}
+                    ItemSeparatorComponent={this.FlatListSeparator}
+                    renderItem={item => this.renderItem(item)}
+                />
+    </View>);
+  };
+
+  GetAction() {
+    this.state = {
+      loading: true,
+      dataSource: []
+    };
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=d7c08717890b402b415ad97c1039fb2c&language=en-US&page=1", {
+      "method": "GET"
+    })
+      .then(response => response.json())
+      .then((responseJson) => {
+          console.log('getting data from fetch', responseJson)
+          this.setState({dataSource: responseJson})
+          setTimeout(() => {
+            this.state = {
+              loading: false,
+              dataSource: responseJson.results[0]
+            };
+            alert(responseJson)
+          }, 4000)
+  
+      })
+      .catch(error => console.log(error))
+  }
+
+  renderItem(data: any) {
+    return (
+        <TouchableOpacity>
+            <Text>{data.item.title}</Text>
+        </TouchableOpacity>
+    )
+  }
+
+  FlatListSeparator() {
+    return (
+        <View style={{
+            height: .5,
+            width: "100%",
+            backgroundColor: "rgba(0,0,0,0.5)",
+        }}
+        />
+    );
+  }
 }
 
-export function GetAction() {
-
-}
+export default GenreScreen;
 
 const styles = StyleSheet.create({
   container: {
